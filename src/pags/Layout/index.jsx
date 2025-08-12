28390f370cfea83f8880624cd7e99b19d84686d2
webWhite <w220640zc@gmail.com>
2025-08-12 09:43:12 +0800

    TabBar切换，路由跳转

--- src/pags/Layout/index.jsx
+++ src/pags/Layout/index.jsx
@@ -1,20 +1,57 @@
-import { Button } from "antd-mobile"
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import { useDispatch } from "react-redux"
 import { useEffect } from "react"
 import { getBillList } from "../../store/modules/billStore"
+import { TabBar } from "antd-mobile"
+import './index.scss'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline
+} from 'antd-mobile-icons'
+
+const tabs = [
+  {
+    key: '/month',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
 
 const Layout = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getBillList())
   },[])
+  // 点击路由跳转
+  const navigate = useNavigate()
+  const changeRouter = (path) => {
+    navigate(path)
+    
+  }
   return (
-    <div>
-      {/* 二级路由出口 */}
-      <Outlet/>
-      this is Layout
-      <Button color='primary'>测试全局</Button>
+    <div className="layout">
+      <div className="container">
+        {/* 二级路由出口 */}
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar onChange={changeRouter}>
+          {tabs.map(item => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
     </div>
   )
 }
